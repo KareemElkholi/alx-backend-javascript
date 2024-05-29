2dbe6af3072b066df3400e4d9664f7bdbe55fbdf
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-05-29 16:02:01 +0300

    All Tasks

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const res = {};
+  try {
+    res.photo = await uploadPhoto();
+    res.user = await createUser();
+  } catch (err) {
+    res.photo = null;
+    res.user = null;
+  }
+  return res;
+}
