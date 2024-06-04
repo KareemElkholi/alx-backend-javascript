bf7ed556616f36204aea7543f512183a22788a51
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-06-05 01:39:27 +0300

    Correction

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (!startString) return '';
   const res = [];
-  set.forEach((el) => { if (typeof value === 'string' && el.startsWith(startString)) res.push(el.slice(startString.length)); });
+  set.forEach((el) => { if (typeof el === 'string' && el.startsWith(startString)) res.push(el.slice(startString.length)); });
   return res.join('-');
 }
