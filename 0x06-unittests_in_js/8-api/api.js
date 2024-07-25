ce390940edaf735297ca9cff53b4a42466d9d0ee
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-07-26 02:49:46 +0300

    Task 8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 7865;
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 app.listen(port, () => {
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,7 +5,7 @@ describe('api test', () => {
     request.get('http://localhost:7865', (_error, response, body) => {
       expect(response.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
+      done();
     });
-    done();
   });
 });
