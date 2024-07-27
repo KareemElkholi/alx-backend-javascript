36d1fb2d80bfeeb4ffa1c14990c263c6d75410d4
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-07-27 04:19:33 +0300

    Task 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+const path = process.argv[2];
+const countStudents = async (path) => {
+  let res = 'This is the list of our students\n';
+  try {
+    let data = await fs.readFile(path, 'utf8');
+    const rows = data.split('\n').filter((row) => row.trim() !== '');
+    const headers = rows[0].split(',').map((header) => header.trim());
+    data = rows.slice(1).map((row) => {
+      const values = row.split(',').map((value) => value.trim());
+      return Object.fromEntries(headers.map((header, i) => [header, values[i]]));
+    });
+    const fields = {};
+    data.forEach((student) => {
+      if (!fields[student.field]) fields[student.field] = [];
+      fields[student.field].push(student.firstname);
+    });
+    res += `Number of students: ${data.length}\n`;
+    for (const [field, names] of Object.entries(fields)) {
+      res += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+    }
+  } catch (error) {
+    res += 'Cannot load the database';
+  }
+  return res;
+};
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.end((await countStudents(path)).trim());
+  }
+}).listen(1245);
+module.exports = app;
